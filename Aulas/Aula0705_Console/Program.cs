376a84276161fde50e8228d88c8735e7bc38ddde
Caue <cauesilvestree@Hotmail.com>
2018-05-10 08:52:24 -0300

    Revert "Teste"
    This reverts commit 27a8e155f4e0a16c595d76ffa9cf7cec58f17776.

--- Aulas/Aula0705_Console/Program.cs
+++ Aulas/Aula0705_Console/Program.cs
@@ -7,16 +7,6 @@ using System.Threading.Tasks;
 namespace Aula0705_Console {
     class Program {
         static void Main(string[] args) {
-            string a = "aaa" ?? "a";
-
-            Console.WriteLine($"Valor de A: { a } ");
-
-
-            Console.WriteLine( Console.CapsLock ? "Ativado" : "Desativado");
-
-            
-
-            Console.ReadLine();
         }
     }
 }
