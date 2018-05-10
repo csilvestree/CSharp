705ed11d36db9c5fe1ee20116eaa93024f782089
Caue <cauesilvestree@Hotmail.com>
2018-05-10 10:24:32 -0300

    Adicionando construtores da classe

--- Aulas/Aula1005_POO/Cliente.cs
+++ Aulas/Aula1005_POO/Cliente.cs
@@ -4,7 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula1005_POO {
-    class Cliente {
+namespace Aula1005_POO
+{
+    class Cliente
+    {
+        //Atributo
+        private string nome;
+
+        //Propriedades
+        public string Cpf { get; set; }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        // Construtores
+        public Cliente() // Construtor vazio
+        {
+
+        }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Cliente(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
+
+
     }
 }
