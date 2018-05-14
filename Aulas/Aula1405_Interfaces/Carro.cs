34e68836f6fd494824af6b806e060f12ca19a5b9
Caue <cauesilvestree@Hotmail.com>
2018-05-14 08:53:51 -0300

    Implementand IMeioTransporte

---
+++ Aulas/Aula1405_Interfaces/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula1405_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+        public Carro()
+        {
+            Velocidade += 10;
+        }
+
+        void IMeioTransporte.Acelerar()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMeioTransporte.Desacelerar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
